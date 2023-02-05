f66560ee4e8fb4d48d394ca95330e4b553b8c93e
Joel Kong <joelkong2001@gmail.com>
2023-02-05 22:24:13 +0800

    feat: implemented skeleton for form

--- biomaker/components/Form.tsx
+++ biomaker/components/Form.tsx
@@ -1,7 +1,26 @@
 import classes from "./header.module.css";
 
 function Form(): JSX.Element {
-  return <div>fsdf</div>;
+  return (
+    <section>
+      <form>
+        <div>
+          <p>What skillsets do you want in your biography?</p>
+          <textarea />
+        </div>
+        <div>
+          <p>
+            What kind of style would you want your biography to be written in?
+          </p>
+        </div>
+        <div>
+          <p>{`Word Count (Optional)`}</p>
+          <input />
+        </div>
+        <button>Generate Bio</button>
+      </form>
+    </section>
+  );
 }
 
 export default Form;
