769f38ea5d2ea398b9c9e68c1a2c22924b469e32
Joel Kong <joelkong2001@gmail.com>
2023-02-09 21:39:20 +0800

    chore: stored form input state

--- biomaker/components/Form.tsx
+++ biomaker/components/Form.tsx
@@ -1,28 +1,64 @@
 import classes from "./form.module.css";
+import { useState } from "react";
 
 function Form(): JSX.Element {
+  //States
+  const [formInput, setFormInput] = useState({
+    skills: "",
+    style: "",
+    wordcount: "",
+  });
+
+  //Store form input state
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormInput({ ...formInput, [name]: value });
+  }
+
+  //Form Submission
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+  }
+
   return (
     <section className={classes.formsection}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={handleSubmit}>
         <div>
-          <p>What skillsets do you want in your biography?</p>
+          <label htmlFor="skills">
+            What skillsets do you want in your biography?
+          </label>
           <input
             className={classes.forminput}
             placeholder="e.g. Programming, Cooking"
+            name="skills"
+            id="skills"
+            onChange={handleChange}
           />
         </div>
         <div>
-          <p className={classes.formquestion}>
+          <label className={classes.formquestion} htmlFor="style">
             What style would you want your biography to be written in?
-          </p>
+          </label>
           <input
             className={classes.forminput}
             placeholder="e.g. Professional, Informal"
+            name="style"
+            id="style"
+            onChange={handleChange}
           />
         </div>
         <div>
-          <p className={classes.formquestion}>{`Word Count (Optional)`}</p>
-          <input className={classes.forminput} />
+          <label
+            className={classes.formquestion}
+            htmlFor="wordcount"
+          >{`Word Count (Optional)`}</label>
+          <input
+            className={classes.forminput}
+            name="wordcount"
+            id="wordcount"
+            onChange={handleChange}
+          />
         </div>
         <div className={classes.formbuttondiv}>
           <button className={classes.formbutton}>Generate Bio</button>
