1bc05ea9f2911ea47302c1344cddaff09b19e782
Joel Kong <joelkong2001@gmail.com>
2023-02-10 21:51:26 +0800

    Delete next.config.js

--- biomaker/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    OPENAI_API_KEY: "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",
-  },
-};
-
-module.exports = nextConfig;
