1ddfb7b61b6aa7b895a0ce78ad5dd96210d61f46
Joel Kong <joelkong2001@gmail.com>
2023-02-10 21:43:30 +0800

    chore: added next.config.js to gitignore

--- biomaker/next.config.js
+++ biomaker/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    OPENAI_API_KEY: "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",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
