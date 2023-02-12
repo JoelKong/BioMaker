1a6dd91255dffc1103cf8d9cfb925919cd7816f0
Joel Kong <joelkong2001@gmail.com>
2023-02-12 11:47:29 +0800

    fix: fixed favicon not appearing on deployed site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,10 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Generate biographies with BioMaker, you can customise the biography to fit your skills and choice of style."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="https://raw.githubusercontent.com/JoelKong/BioMaker/main/public/favicon.ico"
+        />
       </Head>
 
       <Component {...pageProps} />
